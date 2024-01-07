c8ad48c00c80dec5423897a0a081c409f6123177
JonatanMerlo <jonymerlo14@gmail.com>
2024-01-07 11:00:47 -0300

    Agrego cluster

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import express from "express";
+import cluster from "cluster";
+import os from 'os'
 
 import ejerciceRoutes from './routes/ejerciceRoutes.js'
 import routineRoutes from './routes/routineRoutes.js'
 import seriesRoutes from './routes/seriesRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
-const app = express()
-app.use(express.json())
 
+var port = process.env.PORT || '3000';
+const app = express()
+
+
+app.use(express.json())
 
 app.use('/api', ejerciceRoutes)
 app.use('/api', routineRoutes)
 app.use('/api', seriesRoutes)
 app.use('/api', userRoutes)
 
+app.set('port', port);
 
-app.listen(3000)
+if (cluster.isPrimary) {
+    // Count the machine's CPUs
+    let cpuCount = os.cpus().length
+
+    // Create a worker for each CPU
+    for (let i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+} else {
+    app.listen(port)
+    console.log("Server started.");
+}
+
